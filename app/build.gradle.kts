3c763fa35c9d02c867fc3834aa3031d37f3cde0c
Julien DAUVERGNE <judau27@gmail.com>
2025-03-05 15:44:16 +0100

    v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,6 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -26,6 +25,9 @@ android {
             )
         }
     }
+    setProperty("archivesBaseName", "DinoGame")
+
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
